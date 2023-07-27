9bd149a1b207239f2e4300a7501b1926989d1d52
Haf1dzsr <hsramadhan68@gmail.com>
2023-07-27 16:24:37 +0700

    add: splash screen feature

---
+++ lib/presentation/splash/splash_screen.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+import 'package:get/get.dart';
+import 'package:sbakusara_presence_app/domain/core/constants/app_color_styles.dart';
+
+import 'controllers/splash_controller.dart';
+
+class SplashScreen extends GetView<SplashController> {
+  SplashScreen({Key? key}) : super(key: key);
+  @override
+  final SplashController controller = Get.put(SplashController());
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppColorStyle.bgColormain,
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: Center(
+          child: Image.asset(
+            'assets/images/logo_sba.png',
+            fit: BoxFit.cover,
+          ),
+        ),
+      ),
+    );
+  }
+}
