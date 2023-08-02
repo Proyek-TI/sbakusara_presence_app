4bc4bb17824bfad57c1686b7fcf6e974e723b253
Haf1dzsr <hsramadhan68@gmail.com>
2023-08-02 19:31:25 +0700

    init

--- lib/presentation/screens.dart
+++ lib/presentation/screens.dart
@@ -1,6 +1,6 @@
 export 'package:sbakusara_presence_app/presentation/admin_dashboard/admin_dashboard_screen.dart';
 export 'package:sbakusara_presence_app/presentation/auth/auth_screen.dart';
-export 'package:sbakusara_presence_app/presentation/home/user_home_screen.dart';
+export 'package:sbakusara_presence_app/presentation/user_home/user_home_screen.dart';
 export 'package:sbakusara_presence_app/presentation/splash/splash_screen.dart';
 export 'package:sbakusara_presence_app/presentation/user_dashboard/user_dashboard_screen.dart';
 export 'package:sbakusara_presence_app/presentation/user_history/user_history_screen.dart';
