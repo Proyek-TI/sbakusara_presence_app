bcfc1578974faa99ef692cd95b3d62ed6535fd41
Haf1dzsr <hsramadhan68@gmail.com>
2023-08-07 13:48:16 +0700

    add: settings screen

--- lib/presentation/admin_dashboard/controllers/admin_dashboard_controller.dart
+++ lib/presentation/admin_dashboard/controllers/admin_dashboard_controller.dart
@@ -6,9 +6,9 @@ class AdminDashboardController extends GetxController {
   int selectedIndex = 0;
 
   List<Widget> pages = [
-     AdminHomeScreen(),
+    AdminHomeScreen(),
     const Text('Page 2'),
-    const Text('Page 3'),
+    SettingsScreen(),
   ];
 
   void currentIndex(int index) {
